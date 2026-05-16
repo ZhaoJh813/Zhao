4ba9a4f95cc432520f6c81ed14f1de12aeeaab2e
Blog Author <blog@local.dev>
2026-05-16 20:47:17 +0800

    chore: add GitHub Pages deployment workflow

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,5 +4,7 @@ import tailwind from '@astrojs/tailwind';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://zhaojh813.github.io',
+  base: '/Zhao',
   integrations: [tailwind()],
 });
